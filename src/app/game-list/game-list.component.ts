550fed27894d4750c263f9fb3d7d131338c489c2
alessiorong <alessiorongione0598@gmail.com>
2024-08-22 12:12:14 +0200

    modificato gamelist, dove inizialmente mostrerà i risultati delle partite già giocate con la possiblità, premendo un button, di vedere le partite in calendario ancora da giocare

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Game } from '../model/game.model';
 import { GameService } from '../service/game/game.service';
+import { subscribe } from 'diagnostics_channel';
+import { nextTick } from 'process';
 
 @Component({
   selector: 'app-game-list',
@@ -14,7 +16,9 @@ import { GameService } from '../service/game/game.service';
 export class GameListComponent implements OnInit {
 
   gamelist : Game[] = [];
+  filteredGameList: any[] = [];
   teamId! : number;
+  isFilteredByUpcoming: boolean = false;
 
   constructor(
     private gameService : GameService,
@@ -22,18 +26,34 @@ export class GameListComponent implements OnInit {
   ){}
 
   ngOnInit(): void {
-    const teamIdStr = this.route.snapshot.paramMap.get("teamId");
-    if (teamIdStr) {
-      this.teamId = +teamIdStr;
-      this.gameService.getAllGamesByTeam(this.teamId).subscribe({
-        next: (response) => {
-   
-          this.gamelist = response.sort((a:Game, b:Game) => new Date(b.gameDay).getTime() - new Date(a.gameDay).getTime());
-        },
-        error: (err) => {
-          console.error('Errore nel recupero della lista delle partite');
-        }
-      });
-    }
+    this.teamId = +this.route.snapshot.paramMap.get('teamId')!;
+    this.loadGames();
   }
+
+  loadGames(): void {
+    this.gameService.getAllGamesByTeam(this.teamId).subscribe({
+      next: (response) => {
+        this.gamelist = response.sort((a:Game, b:Game) => new Date(b.gameDay).getTime() - new Date(a.gameDay).getTime());
+        this.filterByPlayedGames();
+      },
+      error: (err) => {
+        console.error('Errore nel recupero della lista delle partite');
+      }
+    });
+  }
+
+  filterByPlayedGames(): void {
+    this.filteredGameList = this.gamelist.filter(game => game.result === 'vinta' || game.result === 'persa');
+    this.isFilteredByUpcoming = false; 
+  }
+
+  filterUpcomingGames(): void {
+    this.filteredGameList = this.gamelist
+    .filter(game => game.result === 'da giocare')
+    .sort((a,b) => new Date(a.gameDay).getTime() - new Date(b.gameDay).getTime());
+    this.isFilteredByUpcoming = true;
+  }
+  
+
+
 }
